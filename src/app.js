d99c2012a2e2faef4818b69427c941e626a09ff2
Hosam Fikry <hossam.alaa@yourparts.com>
2020-08-31 20:31:16 +1000

    redirecting root endpoint to public directory

--- src/app.js
+++ src/app.js
@@ -14,26 +14,15 @@ const app = express();
 // bundle of JavaScript). These assets are referred to in the HTML using
 // <link> and <script> tags.
 app.use('/assets', express.static(path.resolve(__dirname, '..', 'dist')));
+//for assignment1 
+app.use('/public', express.static(path.resolve(__dirname, '..', 'public')));
 
 // Set up the index route
 app.get('/', (req, res) => {
-  // The HTML is pretty barebones, it just provides a mount point
-  // for React and links to our styles and scripts.
-  const htmlContent = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
-      </head>
-      <body>
-        <div id="root"></div>
-        <script src="/assets/js/vendor.js"></script>
-        <script src="/assets/js/app.js"></script>
-      </body>
-    </html>`;
 
-  // Respond with the HTML
-  res.send(htmlContent);
+// redirecting to indext.html 
+  res.redirect( 'public/index.html');
+    
 });
 
 // Export the Express app
