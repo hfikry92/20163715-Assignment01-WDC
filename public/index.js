e31a6edf1c83f6c8341c5e400f3859bf1bffa03a
Hosam Fikry <hossam.alaa@yourparts.com>
2020-08-31 21:49:34 +1000

    adding sorting to the table

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+
 const users = [
     {
        uid: 001,
@@ -41,11 +42,20 @@ const users = [
  ]
  
 function generateHTMLTable(users, sortByName){
-   if(sortByName == True){
 
-      users = _.sortBy(users, row => row.name)
+   if (sortByName == true) {
+      users = _.sortBy(users, o => o.personalInfo.name.toLowerCase()) 
    }
    var table = document.getElementById("users_table");
+   table.innerHTML = "";
+   var header = table.createTHead();
+   var row = header.insertRow(0);    
+   var nameCell = row.insertCell(0);
+   nameCell.innerHTML = "<b>Name</b>"; 
+   var emailCell = row.insertCell(1);
+   emailCell.innerHTML = "<b>Email</b>";
+   var stateCell = row.insertCell(2);
+   stateCell.innerHTML = "<b>State</b>";
    var row_i;
    for (row_i = 0; row_i < users.length; row_i++) {
       var row = table.insertRow(row_i+1);
@@ -62,12 +72,11 @@ function generateHTMLTable(users, sortByName){
 
 }
 
- function returnUsers(users) {    
+ function returnUsers(users,sortByName) {    
         var filtered_users = []
         users.forEach(user => {
             filtered_users.push([user.personalInfo.name,user.email,user.personalInfo.address.state])
         });
-        console.log(filtered_users)
-        generateHTMLTable(users)
+        generateHTMLTable(users,sortByName)
         return filtered_users ;
 }
